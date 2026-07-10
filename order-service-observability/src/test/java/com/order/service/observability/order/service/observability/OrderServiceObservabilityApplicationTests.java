5dc02da39b6a5740216db2406872f183d3a327aa
rafaelrsilva00 <idd_rrodrigues@uolinc.com>
2026-07-10 17:41:37 -0300

    Adding Application Test.

---
+++ order-service-observability/src/test/java/com/order/service/observability/order/service/observability/OrderServiceObservabilityApplicationTests.java
@@ -0,0 +1,15 @@
+package com.order.service.observability.order.service.observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class OrderServiceObservabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

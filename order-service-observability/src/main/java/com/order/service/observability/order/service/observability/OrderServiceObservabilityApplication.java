9d79b6f6a27504d911fd191e628a722da391dfbe
rafaelrsilva00 <idd_rrodrigues@uolinc.com>
2026-07-10 17:37:00 -0300

    Add main file application.

---
+++ order-service-observability/src/main/java/com/order/service/observability/order/service/observability/OrderServiceObservabilityApplication.java
@@ -0,0 +1,13 @@
+package com.order.service.observability.order.service.observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceObservabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceObservabilityApplication.class, args);
+	}
+
+}
